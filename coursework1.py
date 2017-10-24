cf3f21be48ca5e52589cb7ab5d95f8642bfbaa1b
SpencerJAl <gpmas117@gmail.com>
2017-10-24 19:00:21 +0000

    Updating before flag add

--- coursework1.py
+++ coursework1.py
@@ -4,163 +4,163 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-  return render_template('index.html')
+  return render_template('index.html'), 200
 
 @app.route('/century')
 def century():
-  return render_template('century.html')
+  return render_template('century.html'), 200
 
 @app.route('/artist')
 def artist():
-  return render_template('artist.php')
+  return render_template('artist.html'), 200
 
 @app.route('/country')
 def country():
-  return render_template('country.html')
+  return render_template('country.html'), 200
 
 @app.route('/artist/gogh')
 def gogh():
-  return render_template('gogh.html')
+  return render_template('gogh.html'), 200
 
 @app.route('/artist/DaVinci')
 def vinci():
-   return render_template('vinci.html')
+   return render_template('vinci.html'), 200
 
 @app.route('/artist/picasso')
 def picasso():
-  return render_template('picasso.html')
+  return render_template('picasso.html'), 200
 
 @app.route('/country/france')
 def france():
-  return render_template('france.html')
+  return render_template('france.html'), 200
 
 @app.route('/country/italy')
 def italy():
-  return render_template('italy.html')
+  return render_template('italy.html'), 200
 
 @app.route('/country/netherlands')
 def netherlands():
-  return render_template('netherlands.html')
+  return render_template('netherlands.html'), 200
 
 @app.route('/century/19th')
 def ninth():
-  return render_template('19th.html')
+  return render_template('19th.html'), 200
 
 @app.route('/century/18th')
 def eighth():
-  return render_template('18th.html')
+  return render_template('18th.html'), 200
 
 @app.route('/century/15th')
 def fifth():
-  return render_template('15th.html')
+  return render_template('15th.html'), 200
 
 @app.route('/artist/vincent')
 def redv():
-  return redirect(url_for('gogh'))
+  return redirect(url_for('gogh')), 301
 
 @app.route('/artist/vincentvan')
 def redvv():
-  return redirect(url_for('gogh'))
+  return redirect(url_for('gogh')), 301
 
 @app.route('/artist/vincentvangogh')
 def redvvg():
-  return redirect(url_for('gogh'))
+  return redirect(url_for('gogh')), 301
 
 @app.route('/artist/vangogh')
 def redvg():
-  return redirect(url_for('gogh'))
+  return redirect(url_for('gogh')), 301
 
 @app.route('/artist/GOGH')
 def redg():
-  return redirect(url_for('gogh'))
+  return redirect(url_for('gogh')), 301
 
 @app.route('/artist/VINCENT')
 def redV():
-  return redirect(url_for('gogh'))
+  return redirect(url_for('gogh')), 301
 
 @app.route('/artist/VINCENTVAN')
 def redVV():
-  return redirect(url_for('gogh'))
+  return redirect(url_for('gogh')), 301
 
 @app.route('/artist/VINCENTVANGOGH')
 def redVVG():
-  return redirect(url_for('gogh'))
+  return redirect(url_for('gogh')), 301
 
 @app.route('/artist/vincent van')
 def redv_v():
-  return redirect(url_for('gogh'))
+  return redirect(url_for('gogh')), 301
 
 @app.route('/artist/vincent van gogh')
 def redv_v_g():
-  return redirect(url_for('gogh'))
+  return redirect(url_for('gogh')), 301
 
 @app.route('/artist/VINCENT VAN')
 def redV_V():
-  return redirect(url_for('gogh'))
+  return redirect(url_for('gogh')), 301
 
 @app.route('/artist/VINCENT VAN GOGH')
 def redV_V_G():
-  return redirect(url_for('gogh'))
+  return redirect(url_for('gogh')), 301
 
 @app.route('/artist/pablo')
 def redpa():
-  return redirect(url_for('picasso'))
+  return redirect(url_for('picasso')), 301
 
 @app.route('/artist/PABLO')
 def redPA():
-  return redirect(url_for('picasso'))
+  return redirect(url_for('picasso')), 301
 
 @app.route('/artist/Pablo')
 def redPa():
-  return redirect(url_for('picasso'))
+  return redirect(url_for('picasso')), 301
 
 @app.route('/artist/pablo picasso')
 def redpp():
-  return redirect(url_for('picasso'))
+  return redirect(url_for('picasso')), 301
 
-@app.route('/artist/PABLO PICASSP')
+@app.route('/artist/PABLO PICASSO')
 def redPP():
-  return redirect(url_for('picasso'))
+  return redirect(url_for('picasso')), 301
 
 @app.route('/artist/Pablo Picasso')
 def redPaPi():
-  return redirect(url_for('gogh'))
+  return redirect(url_for('picasso')), 301
 
 @app.route('/artist/piccaso')
 def redp():
-  return redirect(url_for('picasso'))
+  return redirect(url_for('picasso')), 301
 
 @app.route('/artist/da vinci')
 def redda():
-  return redirect(url_for('DaVinci'))
+  return redirect(url_for('DaVinci')), 301
 
 @app.route('/artist/DA VINCI')
 def redDA():
-  return redirect(url_for('DaVinci'))
+  return redirect(url_for('DaVinci')), 301
 
 @app.route('/artist/leonardo')
 def redleo():
-  return redirect(url_for('DaVinci'))
+  return redirect(url_for('DaVinci')), 301
 
 @app.route('/artist/Leonardo')
 def redLeo():
-  return redirect(url_for('DaVinci'))
+  return redirect(url_for('DaVinci')), 301
 
 @app.route('/artist/LEONARDO')
 def redLEO():
-  return redirect(url_for('DaVinci'))
+  return redirect(url_for('DaVinci')), 301
 
 @app.route('/artist/leonardo da vinci')
 def redleod():
-  return redirect(url_for('DaVinci'))
+  return redirect(url_for('DaVinci')), 301
 
 @app.route('/artist/Leonardo Da Vinci')
 def redLeoD():
-  return redirect(url_for('DaVinvi'))
+  return redirect(url_for('DaVinvi')), 301
 
 @app.route('/artist/LEONARDO DA VINCI')
 def redLEOD():
-  return redirect(url_for('DaVinci'))
+  return redirect(url_for('DaVinci')), 301
 
 
 @app.route('/config/')
